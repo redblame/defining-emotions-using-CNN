8dfc3b9065b3dec5185767651cdd036d24885683
iamesenya <bard.rage@mail.ru>
2023-01-31 21:10:35 +0400

    Add files via upload

---
+++ TrainEmotionDetector.py
@@ -0,0 +1,67 @@
+
+# импортируйте необходимые пакеты
+import cv2
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+
+# инициализируйте генератор данных изображения с помощью масштабирования
+train_data_gen = ImageDataGenerator(rescale=1./255)
+validation_data_gen = ImageDataGenerator(rescale=1./255)
+
+# предварительная обработка всех тестовых изображений
+train_generator = train_data_gen.flow_from_directory(
+        'data/train',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# предварительная обработка всех обучающих изображений
+validation_generator = validation_data_gen.flow_from_directory(
+        'data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# создание структуры модели
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+
+# обучаем нейронную сеть/модель
+emotion_model_info = emotion_model.fit_generator(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+
+# сохранить структуру модели в файле json
+model_json = emotion_model.to_json()
+with open("emotion_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+# сохранить вес обученной модели в файле .h5
+emotion_model.save_weights('emotion_model.h5')
+
